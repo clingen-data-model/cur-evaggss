bab5519c3c018a72103067f64099b86292cfe343
Benjamin Blankenmeister <bblanken@broadinstitute.org>
2025-11-21 18:22:02 -0500

    this one is text

--- lib/evagg/library/rare_disease.py
+++ lib/evagg/library/rare_disease.py
@@ -106,7 +106,7 @@ class RareDiseaseFileLibrary(IGetPapers):
             user_prompt_file=_get_prompt_file_path("paper_category"),
             system_prompt="Extract field",
             params=parameters,
-            prompt_settings={"prompt_tag": "paper_category", "prompt_metadata": prompt_metadata, "temperature": 0.8},
+            prompt_settings={"prompt_tag": "paper_category", "prompt_metadata": prompt_metadata, "temperature": 0.8, "response_format": {"type": "text"}},
         )
 
         result = response.strip('"')
